0b104794d51142b7eb042f56dc6f96966bfee4be
undert86 <112851776+undert86@users.noreply.github.com>
2023-04-05 14:26:00 +0300

    Update bottg.py

--- bottg.py
+++ bottg.py
@@ -1,85 +1,65 @@
-import telebot
 import os
 import spotipy
-import spotipy.util as util
-from telebot import types
-import sys
-from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.oauth2 as oauth2
+import telebot
+import requests
+import io
+# Получаем credentials для Spotify API
+credentials = oauth2.SpotifyClientCredentials(
+    client_id='e9f794eb35d8483d86dc0bd8b04dc76e',
+    client_secret='888651fbda704dcdaf387368b554d871'
+)
+sp = spotipy.Spotify(client_credentials_manager=credentials)
 
-token = "6104472904:AAGdgAniuOY3zHf-7OXН5SAlNV_GmtcnRM0"
-bot = telebot.TeleBot(token)
+# Получаем токен бота
+BOT_TOKEN = '6104472904:AAGdgAniuOY3zHf-7OXH5SAlNV_GmtcnRM0'
+bot = telebot.TeleBot(BOT_TOKEN)
 
-client_id = "e9f7a94eb35d483d86dc0bd8b04dc76e"
-client_secret = "888651fbda704dcdaf387368b554d871"
-REDIRECTURI = 'http://localhost:8888/callback'
-
-# Права доступа к API Spotify
-SCOPE = 'user-library-read'
-
-
-def authorize_spotify(user_id):
-    token = util.prompt_for_user_token(user_id, SCOPE, clientid, client_secret, REDIRECT_URI)
-    sp = spotipy.Spotify(auth=token)
-    return sp
-
-
+# Обработчик команды start
 @bot.message_handler(commands=['start'])
-def start(update):
-    user_id = update.chat.id
-    sp = authorize_spotify(user_id)
-    if sp:
-        message = "Привет, это бот, который импортирует песни из заблокированной на территории РФ площадки - Spotify. Для начала авторизуйтесь в Spotify, отправив мне команду /auth. Напиши /help, чтобы ознакомиться с командами"
+def start_message(message):
+    text = "Привет! Я могу искать треки на Spotify. Введите название исполнителя и трека через знак '-'.\nНапример: Ariana Grande - No Tears Left To Cry"
+    bot.send_message(message.chat.id, text)
+
+# Обработчик текстовых сообщений
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    text = message.text
+    print(f"User's request: {text}")
+    items = text.split('-')
+    if len(items) != 2:
+        bot.reply_to(message, 'Введите название исполнителя и трека через знак "-".')
     else:
-        message = "Не удалось авторизоваться в Spotify. Проверьте настройки приложения"
-    bot.send_message(update.chat.id, message)
+        artist = items[0].strip()
+        track = items[1].strip()
+        # Ищем трек на Spotify
+        query = f"artist:{artist} track:{track}"
+        result = sp.search(q=query, type='track', limit=1)
+        if result['tracks']['items']:
+            track = result['tracks']['items'][0]
+            track_preview = track['preview_url']
+            track_name = track['name']
+            track_artist = track['artists'][0]['name']
+            track_cover_url = track['album']['images'][0]['url']
+            download_button = telebot.types.InlineKeyboardButton('Скачать', url=track_preview)
+            # Создаем объект клавиатуры и добавляем на нее кнопку "Скачать"
+            keyboard = telebot.types.InlineKeyboardMarkup()
+            keyboard.add(download_button)
+            # Загружаем обложку трека и отправляем ее вместе с аудиофайлом
+            response = requests.get(track_cover_url, headers={'Content-Type': 'image/jpeg'})
+            if response.status_code == 200:
+                photo = io.BytesIO(response.content)
+                bot.send_photo(message.chat.id, photo, caption=f'{track_artist} - {track_name}')
+                bot.send_audio(message.chat.id, track_preview, title=track_name, performer=track_artist, reply_markup=keyboard)
+        else:
+            bot.reply_to(message, 'К сожалению, я не нашел такой трек на Spotify.')
 
 
-# Функция для обработки команды /auth
-@bot.message_handler(commands=['auth'])
-def auth(update):
-    user_id = update.chatid
-    sp = authorize_spotify(user_id)
-    if sp:
-        message = 'Вы успешно авторизовались в Spotify. Теперь вы можете искать музыку. Напиши /findsong, там дальнейшая инструкция'
-    else:
-        message = 'Не удалось авторизоваться в Spotify. Проверьте настройки приложения.'
-    bot.send_message(update.chat.id, message)
+# Запускаем бота
+def main():
+    """Запуск бота."""
+    print('стартуем..')
+    bot.polling()
 
-
-
-@bot.message_handler(commands['help'])
-def get_text_messages(message):
-    bot.send_message(message.from_user.id,
-                     "/findsong - найти песню по названию и исполнителю, /findmus - найти исполнителя")
-
-
-@bot.message_handler(func=lambda message: True)
-def import_song(message):
-    user_id = message.chat.id
-    sp = authorize_spotify(user_id)
-
-    # Получение названия песни и исполнителя из сообщения пользователя
-    query = ' '.join(message.text.split()[1:])
-    # Разделение названия песни и исполнителя на две переменные
-    song_name, artist_name = None, None
-    for word in query.split():
-        if word.lower() == 'by':
-            index = query.index(word)
-            song_name = query[:index].strip()
-            artist_name = query[index+2:].strip()
-            break
-
-    # Поиск песни в Spotify
-    # Если названия песни и исполнителя не найдены, то сообщаем об ошибке
-    if song_name is None or artist_name is None:
-        bot.send_message(message.chat.id,
-                         'Неверный формат запроса. Попробуйте еще раз, указав название и исполнителя через "by".')
-        return
-
-    # Проверка наличия результатов поиска и отправка ссылки на предварительный просмотр песни пользователю
-    if len(results['tracks']['items']) > 0:
-        track = results['tracks']['items'][0]
-        preview_url = track['preview_url']
-        bot.send_message(message.chat.id, 'Вот твоя песня: {}'.format(preview_url))
-    else:
-        bot.send_message(message.chat.id, 'К сожалению, я не смог найти эту песню.')
+if __name__ == '__main__':
+    main()
